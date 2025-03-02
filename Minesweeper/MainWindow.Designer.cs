5b396319d95f67a6db42bacda309b05b2cb520e6
David Frischknecht <fishnet37222@gmail.com>
2025-03-02 13:46:43 -0500

    MainWindow: Add the New Game button and associated bitmaps.

--- Minesweeper/MainWindow.Designer.cs
+++ Minesweeper/MainWindow.Designer.cs
@@ -32,22 +32,73 @@ partial class MainWindow
 	/// </summary>
 	private void InitializeComponent()
 	{
+		ToolStripMenuItem gameToolStripMenuItem;
+		ToolStripMenuItem helpToolStripMenuItem;
 		var resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+		this.m_menuStrip = new MenuStrip();
+		this.m_btnNewGame = new Button();
+		gameToolStripMenuItem = new ToolStripMenuItem();
+		helpToolStripMenuItem = new ToolStripMenuItem();
+		this.m_menuStrip.SuspendLayout();
 		this.SuspendLayout();
+		// 
+		// gameToolStripMenuItem
+		// 
+		gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+		gameToolStripMenuItem.Size = new Size(50, 20);
+		gameToolStripMenuItem.Text = "&Game";
+		// 
+		// helpToolStripMenuItem
+		// 
+		helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+		helpToolStripMenuItem.Size = new Size(44, 20);
+		helpToolStripMenuItem.Text = "&Help";
+		// 
+		// m_menuStrip
+		// 
+		this.m_menuStrip.Items.AddRange(new ToolStripItem[] { gameToolStripMenuItem, helpToolStripMenuItem });
+		this.m_menuStrip.Location = new Point(0, 0);
+		this.m_menuStrip.Name = "m_menuStrip";
+		this.m_menuStrip.RenderMode = ToolStripRenderMode.System;
+		this.m_menuStrip.Size = new Size(800, 24);
+		this.m_menuStrip.TabIndex = 0;
+		this.m_menuStrip.Text = "menuStrip1";
+		// 
+		// m_btnNewGame
+		// 
+		this.m_btnNewGame.Anchor = AnchorStyles.Top;
+		this.m_btnNewGame.AutoSize = true;
+		this.m_btnNewGame.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+		this.m_btnNewGame.Image = Properties.Resources.Smile1;
+		this.m_btnNewGame.Location = new Point(378, 27);
+		this.m_btnNewGame.Name = "m_btnNewGame";
+		this.m_btnNewGame.Padding = new Padding(3);
+		this.m_btnNewGame.Size = new Size(44, 44);
+		this.m_btnNewGame.TabIndex = 1;
+		this.m_btnNewGame.UseVisualStyleBackColor = true;
 		// 
 		// MainWindow
 		// 
 		this.AutoScaleDimensions = new SizeF(7F, 15F);
 		this.AutoScaleMode = AutoScaleMode.Font;
 		this.ClientSize = new Size(800, 450);
+		this.Controls.Add(this.m_btnNewGame);
+		this.Controls.Add(this.m_menuStrip);
 		this.FormBorderStyle = FormBorderStyle.FixedSingle;
 		this.Icon = (Icon)resources.GetObject("$this.Icon");
+		this.MainMenuStrip = this.m_menuStrip;
 		this.MaximizeBox = false;
 		this.Name = "MainWindow";
 		this.Text = "Minesweeper";
 		this.Load += this.MainWindow_Load;
+		this.m_menuStrip.ResumeLayout(false);
+		this.m_menuStrip.PerformLayout();
 		this.ResumeLayout(false);
+		this.PerformLayout();
 	}
 
 	#endregion
+
+	private MenuStrip m_menuStrip;
+	private Button m_btnNewGame;
 }
