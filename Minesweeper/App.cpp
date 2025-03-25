a0650577e70612ef8025ff5796d6bab65b197c08
David Frischknecht <fishnet37222@gmail.com>
2025-03-25 10:02:25 -0400

    Initial commit.

---
+++ Minesweeper/App.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2025 David A. Frischknecht
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "pch.h"
+#include "App.h"
+#include "MainWindow.h"
+
+wxIMPLEMENT_APP(App);
+
+bool App::OnInit()
+{
+	if (!wxApp::OnInit()) return false;
+
+	SetVendorName("FishNetSoft");
+	SetAppName("Minesweeper");
+
+	auto* mainWindow = new MainWindow();
+	SetTopWindow(mainWindow);
+	mainWindow->Show();
+
+	return true;
+}
