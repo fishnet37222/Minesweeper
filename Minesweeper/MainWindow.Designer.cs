7807d06d6d907378232cf68b31cc4294f57de0ff
David Frischknecht <fishnet37222@gmail.com>
2025-04-03 15:04:03 -0400

    Initial commit.

---
+++ Minesweeper/MainWindow.Designer.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) 2025 David A. Frischknecht
+//
+// SPDX-License-Identifier: Apache-2.0
+
+namespace Minesweeper;
+
+partial class MainWindow
+{
+	private System.ComponentModel.IContainer components = null;
+
+	protected override void Dispose(bool disposing)
+	{
+		if (disposing && (components != null))
+		{
+			components.Dispose();
+		}
+		base.Dispose(disposing);
+	}
+
+	#region Windows Form Designer generated code
+
+	private void InitializeComponent()
+	{
+		var resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+		this.SuspendLayout();
+		// 
+		// MainWindow
+		// 
+		this.AutoScaleDimensions = new SizeF(7F, 15F);
+		this.AutoScaleMode = AutoScaleMode.Font;
+		this.ClientSize = new Size(800, 450);
+		this.FormBorderStyle = FormBorderStyle.FixedSingle;
+		this.Icon = (Icon)resources.GetObject("$this.Icon");
+		this.Name = "MainWindow";
+		this.Text = "Minesweeper";
+		this.ResumeLayout(false);
+	}
+
+	#endregion
+}
---
+++ Minesweeper/MainWindow.cs
@@ -0,0 +1,13 @@
+// Copyright (c) 2025 David A. Frischknecht
+//
+// SPDX-License-Identifier: Apache-2.0
+
+namespace Minesweeper;
+
+public partial class MainWindow : Form
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}
