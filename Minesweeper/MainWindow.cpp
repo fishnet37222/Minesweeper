9c569250ac19cb957ab2432eeb92c26c9eb1ed42
David Frischknecht <fishnet37222@gmail.com>
2025-04-13 11:54:41 -0400

    Add menu functionality to MainWindow
    Introduce a new enumeration `MenuId` for menu item IDs,
    create a `wxMenuBar` with "Game" and "Help" menus,
    and implement the `MenuBar_OnItemSelect` function
    to handle menu selections, including exiting the application.
    Update `MainWindow` class to include a private member
    for the menu bar and the new event handler method.

--- Minesweeper/MainWindow.cpp
+++ Minesweeper/MainWindow.cpp
@@ -6,6 +6,19 @@
 #include "MainWindow.h"
 #include "App.h"
 
+enum MenuId : uint16_t
+{
+	ID_GAME_NEW = wxID_HIGHEST + 1,
+	ID_GAME_BEGINNER,
+	ID_GAME_INTERMEDIATE,
+	ID_GAME_EXPERT,
+	ID_GAME_CUSTOM,
+	ID_GAME_BEST_TIMES,
+	ID_GAME_EXIT,
+	ID_HELP_HOW_TO_PLAY,
+	ID_HELP_ABOUT
+};
+
 MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, wxGetApp().GetAppName(), wxDefaultPosition, wxDefaultSize,
 	wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX))
 {
@@ -13,5 +26,46 @@ MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, wxGetApp().GetAppName(), w
 
 	wxFrame::SetIcons({ "APP_ICON", nullptr });
 
+	m_menuBar = new wxMenuBar();
+
+	auto* mnuGame = new wxMenu();
+	mnuGame->Append(ID_GAME_NEW, "&New\tF2");
+	mnuGame->AppendSeparator();
+	mnuGame->AppendCheckItem(ID_GAME_BEGINNER, "&Beginner");
+	mnuGame->AppendCheckItem(ID_GAME_INTERMEDIATE, "&Intermediate");
+	mnuGame->AppendCheckItem(ID_GAME_EXPERT, "&Expert");
+	mnuGame->AppendCheckItem(ID_GAME_CUSTOM, "&Custom...");
+	mnuGame->AppendSeparator();
+	mnuGame->Append(ID_GAME_BEST_TIMES, "Best &Times...");
+	mnuGame->AppendSeparator();
+	mnuGame->Append(ID_GAME_EXIT, "E&xit", "Exit");
+	m_menuBar->Append(mnuGame, "&Game");
+
+	auto* mnuHelp = new wxMenu();
+	mnuHelp->Append(ID_HELP_HOW_TO_PLAY, "How to &Play...");
+	mnuHelp->AppendSeparator();
+	mnuHelp->Append(ID_HELP_ABOUT, "&About Minesweeper...");
+	m_menuBar->Append(mnuHelp, "&Help");
+
+	wxFrame::SetMenuBar(m_menuBar);
+	m_menuBar->Bind(wxEVT_MENU, &MainWindow::MenuBar_OnItemSelect, this);
+
 	CenterOnScreen();
+}
+
+// ReSharper disable once CppParameterMayBeConstPtrOrRef
+void MainWindow::MenuBar_OnItemSelect(wxCommandEvent& event)
+{
+	switch (event.GetId())
+	{
+		case ID_GAME_EXIT:
+		{
+			Close();
+
+			break;
+		}
+
+		default:
+			break;
+	}
 }
